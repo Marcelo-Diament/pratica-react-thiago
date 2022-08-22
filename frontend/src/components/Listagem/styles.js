1c0248478dbbc83b87cd31bfc480efb0f6a04c27
Marcelo Diament <marcelo.diament@driven.cx>
2022-08-22 20:05:52 -0300

    feat: adiciona o componente Card

--- frontend/src/components/Listagem/styles.js
+++ frontend/src/components/Listagem/styles.js
@@ -3,6 +3,33 @@ import styled from "styled-components";
 export const ListaDeProdutos = styled.ul`
   display: grid;
   grid-gap: 20px;
-  grid-template-columns: repeat(auto-fit, minmax(400px, 1fr));
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
   list-style: none;
+`;
+
+export const Card = styled.li`
+  font-size: 18px;
+
+  img {
+    max-height: 200px;
+    width: 100%;
+  }
+
+  .descricao {
+    color: #555;
+    font-size: 16px;
+  }
+
+  .preco {
+    font-size: 18px;
+    font-weight: bold;
+  }
+
+  button {
+    background-color: yellowgreen;
+    border-radius: 6px;
+    color: #000;
+    font-weight: bold;
+    padding: 6px 12px;
+  }
 `;
