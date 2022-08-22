bb4b7268374ace835528a8300d376f14bc5c9fd6
Marcelo Diament <marcelo.diament@driven.cx>
2022-08-22 20:56:35 -0300

    feat: adiciona consumo à API backend com axios para capturar produtos

--- frontend/src/components/Listagem/index.js
+++ frontend/src/components/Listagem/index.js
@@ -1,39 +1,32 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
 import { ListaDeProdutos, Card } from "./styles";
 const ListagemDeProdutos = () => {
+  const [produtos, setProdutos] = useState([]);
+
+  const buscarProdutos = async () => {
+    const { data } = await axios.get("http://localhost:8000/produtos");
+    setProdutos(data);
+  };
+  useEffect(() => {
+    buscarProdutos();
+  }, []);
+
   return (
     <main>
       <h3>Produtos</h3>
       <ListaDeProdutos>
-        <Card>
-          <img
-            alt="Fone de Ouvido"
-            src="https://s2.glbimg.com/mma8-1NK-jok6W2NVY1eoRKlxW0=/0x0:1920x1440/924x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2020/8/6/XvaNKAQWeDwYnmL3itCQ/fone-de-ouvido-da-apple.jpg"
-          />
-          <h4>Fone de ouvido</h4>
-          <p className="descricao">Fone de ouvido branco, com fio</p>
-          <p className="preco">R$ 70,00</p>
-          <button>Comprar</button>
-        </Card>
-        <Card>
-          <img
-            alt="Fone de Ouvido"
-            src="https://s2.glbimg.com/mma8-1NK-jok6W2NVY1eoRKlxW0=/0x0:1920x1440/924x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2020/8/6/XvaNKAQWeDwYnmL3itCQ/fone-de-ouvido-da-apple.jpg"
-          />
-          <h4>Fone de ouvido</h4>
-          <p className="descricao">Fone de ouvido branco, com fio</p>
-          <p className="preco">R$ 70,00</p>
-          <button>Comprar</button>
-        </Card>
-        <Card>
-          <img
-            alt="Fone de Ouvido"
-            src="https://s2.glbimg.com/mma8-1NK-jok6W2NVY1eoRKlxW0=/0x0:1920x1440/924x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2020/8/6/XvaNKAQWeDwYnmL3itCQ/fone-de-ouvido-da-apple.jpg"
-          />
-          <h4>Fone de ouvido</h4>
-          <p className="descricao">Fone de ouvido branco, com fio</p>
-          <p className="preco">R$ 70,00</p>
-          <button>Comprar</button>
-        </Card>
+        {produtos.map((produto) => {
+          return (
+            <Card key={produto.id}>
+              <img alt={produto.nome} src={produto.foto} />
+              <h4>{produto.nome}</h4>
+              <p className="descricao">{produto.descricao}</p>
+              <p className="preco">{produto.preco}</p>
+              <button>Comprar</button>
+            </Card>
+          );
+        })}
       </ListaDeProdutos>
     </main>
   );
