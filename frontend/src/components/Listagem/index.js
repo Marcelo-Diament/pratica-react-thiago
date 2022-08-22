21f6798f8bbea2ca0a8dbe649043f61d39b5dcde
Marcelo Diament <marcelo.diament@driven.cx>
2022-08-22 19:48:49 -0300

    feat: adiciona itens ao HTML de ListagemDeProdutos

--- frontend/src/components/Listagem/index.js
+++ frontend/src/components/Listagem/index.js
@@ -1,8 +1,169 @@
 const ListagemDeProdutos = () => {
   return (
     <main>
-      <h2>Produtos</h2>
-      <ul></ul>
+      <h3>Produtos</h3>
+      <ul>
+        <li>
+          <img
+            alt="Fone de Ouvido"
+            src="https://s2.glbimg.com/mma8-1NK-jok6W2NVY1eoRKlxW0=/0x0:1920x1440/924x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2020/8/6/XvaNKAQWeDwYnmL3itCQ/fone-de-ouvido-da-apple.jpg"
+          />
+          <h4>Fone de ouvido</h4>
+          <p>Fone de ouvido branco, com fio</p>
+          <p>R$ 70,00</p>
+          <button>Comprar</button>
+        </li>
+        <li>
+          <img
+            alt="Fone de Ouvido"
+            src="https://s2.glbimg.com/mma8-1NK-jok6W2NVY1eoRKlxW0=/0x0:1920x1440/924x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2020/8/6/XvaNKAQWeDwYnmL3itCQ/fone-de-ouvido-da-apple.jpg"
+          />
+          <h4>Fone de ouvido</h4>
+          <p>Fone de ouvido branco, com fio</p>
+          <p>R$ 70,00</p>
+          <button>Comprar</button>
+        </li>
+        <li>
+          <img
+            alt="Fone de Ouvido"
+            src="https://s2.glbimg.com/mma8-1NK-jok6W2NVY1eoRKlxW0=/0x0:1920x1440/924x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2020/8/6/XvaNKAQWeDwYnmL3itCQ/fone-de-ouvido-da-apple.jpg"
+          />
+          <h4>Fone de ouvido</h4>
+          <p>Fone de ouvido branco, com fio</p>
+          <p>R$ 70,00</p>
+          <button>Comprar</button>
+        </li>
+        <li>
+          <img
+            alt="Fone de Ouvido"
+            src="https://s2.glbimg.com/mma8-1NK-jok6W2NVY1eoRKlxW0=/0x0:1920x1440/924x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2020/8/6/XvaNKAQWeDwYnmL3itCQ/fone-de-ouvido-da-apple.jpg"
+          />
+          <h4>Fone de ouvido</h4>
+          <p>Fone de ouvido branco, com fio</p>
+          <p>R$ 70,00</p>
+          <button>Comprar</button>
+        </li>
+        <li>
+          <img
+            alt="Fone de Ouvido"
+            src="https://s2.glbimg.com/mma8-1NK-jok6W2NVY1eoRKlxW0=/0x0:1920x1440/924x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2020/8/6/XvaNKAQWeDwYnmL3itCQ/fone-de-ouvido-da-apple.jpg"
+          />
+          <h4>Fone de ouvido</h4>
+          <p>Fone de ouvido branco, com fio</p>
+          <p>R$ 70,00</p>
+          <button>Comprar</button>
+        </li>
+        <li>
+          <img
+            alt="Fone de Ouvido"
+            src="https://s2.glbimg.com/mma8-1NK-jok6W2NVY1eoRKlxW0=/0x0:1920x1440/924x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2020/8/6/XvaNKAQWeDwYnmL3itCQ/fone-de-ouvido-da-apple.jpg"
+          />
+          <h4>Fone de ouvido</h4>
+          <p>Fone de ouvido branco, com fio</p>
+          <p>R$ 70,00</p>
+          <button>Comprar</button>
+        </li>
+        <li>
+          <img
+            alt="Fone de Ouvido"
+            src="https://s2.glbimg.com/mma8-1NK-jok6W2NVY1eoRKlxW0=/0x0:1920x1440/924x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2020/8/6/XvaNKAQWeDwYnmL3itCQ/fone-de-ouvido-da-apple.jpg"
+          />
+          <h4>Fone de ouvido</h4>
+          <p>Fone de ouvido branco, com fio</p>
+          <p>R$ 70,00</p>
+          <button>Comprar</button>
+        </li>
+        <li>
+          <img
+            alt="Fone de Ouvido"
+            src="https://s2.glbimg.com/mma8-1NK-jok6W2NVY1eoRKlxW0=/0x0:1920x1440/924x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2020/8/6/XvaNKAQWeDwYnmL3itCQ/fone-de-ouvido-da-apple.jpg"
+          />
+          <h4>Fone de ouvido</h4>
+          <p>Fone de ouvido branco, com fio</p>
+          <p>R$ 70,00</p>
+          <button>Comprar</button>
+        </li>
+        <li>
+          <img
+            alt="Fone de Ouvido"
+            src="https://s2.glbimg.com/mma8-1NK-jok6W2NVY1eoRKlxW0=/0x0:1920x1440/924x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2020/8/6/XvaNKAQWeDwYnmL3itCQ/fone-de-ouvido-da-apple.jpg"
+          />
+          <h4>Fone de ouvido</h4>
+          <p>Fone de ouvido branco, com fio</p>
+          <p>R$ 70,00</p>
+          <button>Comprar</button>
+        </li>
+        <li>
+          <img
+            alt="Fone de Ouvido"
+            src="https://s2.glbimg.com/mma8-1NK-jok6W2NVY1eoRKlxW0=/0x0:1920x1440/924x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2020/8/6/XvaNKAQWeDwYnmL3itCQ/fone-de-ouvido-da-apple.jpg"
+          />
+          <h4>Fone de ouvido</h4>
+          <p>Fone de ouvido branco, com fio</p>
+          <p>R$ 70,00</p>
+          <button>Comprar</button>
+        </li>
+        <li>
+          <img
+            alt="Fone de Ouvido"
+            src="https://s2.glbimg.com/mma8-1NK-jok6W2NVY1eoRKlxW0=/0x0:1920x1440/924x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2020/8/6/XvaNKAQWeDwYnmL3itCQ/fone-de-ouvido-da-apple.jpg"
+          />
+          <h4>Fone de ouvido</h4>
+          <p>Fone de ouvido branco, com fio</p>
+          <p>R$ 70,00</p>
+          <button>Comprar</button>
+        </li>
+        <li>
+          <img
+            alt="Fone de Ouvido"
+            src="https://s2.glbimg.com/mma8-1NK-jok6W2NVY1eoRKlxW0=/0x0:1920x1440/924x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2020/8/6/XvaNKAQWeDwYnmL3itCQ/fone-de-ouvido-da-apple.jpg"
+          />
+          <h4>Fone de ouvido</h4>
+          <p>Fone de ouvido branco, com fio</p>
+          <p>R$ 70,00</p>
+          <button>Comprar</button>
+        </li>
+        <li>
+          <img
+            alt="Fone de Ouvido"
+            src="https://s2.glbimg.com/mma8-1NK-jok6W2NVY1eoRKlxW0=/0x0:1920x1440/924x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2020/8/6/XvaNKAQWeDwYnmL3itCQ/fone-de-ouvido-da-apple.jpg"
+          />
+          <h4>Fone de ouvido</h4>
+          <p>Fone de ouvido branco, com fio</p>
+          <p>R$ 70,00</p>
+          <button>Comprar</button>
+        </li>
+        <li>
+          <img
+            alt="Fone de Ouvido"
+            src="https://s2.glbimg.com/mma8-1NK-jok6W2NVY1eoRKlxW0=/0x0:1920x1440/924x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2020/8/6/XvaNKAQWeDwYnmL3itCQ/fone-de-ouvido-da-apple.jpg"
+          />
+          <h4>Fone de ouvido</h4>
+          <p>Fone de ouvido branco, com fio</p>
+          <p>R$ 70,00</p>
+          <button>Comprar</button>
+        </li>
+        <li>
+          <img
+            alt="Fone de Ouvido"
+            src="https://s2.glbimg.com/mma8-1NK-jok6W2NVY1eoRKlxW0=/0x0:1920x1440/924x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2020/8/6/XvaNKAQWeDwYnmL3itCQ/fone-de-ouvido-da-apple.jpg"
+          />
+          <h4>Fone de ouvido</h4>
+          <p>Fone de ouvido branco, com fio</p>
+          <p>R$ 70,00</p>
+          <button>Comprar</button>
+        </li>
+        <li>
+          <img
+            alt="Fone de Ouvido"
+            src="https://s2.glbimg.com/mma8-1NK-jok6W2NVY1eoRKlxW0=/0x0:1920x1440/924x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2020/8/6/XvaNKAQWeDwYnmL3itCQ/fone-de-ouvido-da-apple.jpg"
+          />
+          <h4>Fone de ouvido</h4>
+          <p>Fone de ouvido branco, com fio</p>
+          <p>R$ 70,00</p>
+          <button>Comprar</button>
+        </li>
+      </ul>
     </main>
   );
 };
