c5fe079ae776d7d6d2172ba497ab944d36618711
Marcelo Diament <marcelo.diament@driven.cx>
2022-08-22 21:15:18 -0300

    feat: limpando CriarProduto

--- frontend/src/pages/CriarProduto/index.js
+++ frontend/src/pages/CriarProduto/index.js
@@ -1,12 +1,10 @@
 import Header from "../../components/Header";
-import ListagemDeProdutos from "../../components/Listagem";
 
 function CriarProduto() {
   return (
     <>
       <Header />
       <h1>CRIAR PRODUTO</h1>
-      <ListagemDeProdutos />
     </>
   );
 }
